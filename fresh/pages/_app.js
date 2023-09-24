7594c8561323fdf877d8d5e7d496a579ed8215b1
alienorder <alienorder.dev@gmail.com>
2023-09-24 23:51:23 +0900

    #2

--- fresh/pages/_app.js
+++ fresh/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <div className="navbar">
+        <Link href="/">홈</Link>
+        <Link href="/list">List</Link>
+      </div>
+      <Component {...pageProps} />
+    </div>
+  )
 }
