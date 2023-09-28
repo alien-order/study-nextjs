ce714dd31159bd17da4c20838a1ba7b7f8e9fbd6
alienorder.dev <alienorder.dev@gmail.com>
2023-09-28 14:25:39 +0900

    #3

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '@/styles/globals.css'
+import Link from 'next/link'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div>
+      <div className="navbar">
+        <Link href="/">홈</Link>
+        <Link href="/list">List</Link>
+        <Link href="/cart">Cart</Link>
+      </div>
+      <Component {...pageProps} />
+    </div>
+  )
+}
---
+++ pages/list.js
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+export default function List() {
+    let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+
+    return (
+      <div>
+        <h4 className="title">상품목록</h4>
+        {
+            상품.map((a, i) => 
+              <div className="food" key={ i }>
+                <img src={`/food${ i }.png`} className="food-img"/>
+                <h4>{ a } 40$</h4>
+              </div>
+            )
+        }
+      </div>
+    )
+  }
